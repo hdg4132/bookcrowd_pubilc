438ab41943e93ec200db72bf4b810afbc9e5231f
KWJ5668 <tocando12@gmail.com>
2024-08-06 14:55:47 +0900

    240806 kwj back

---
+++ src/main/java/com/example/demo3/persistence/CommunityRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo3.persistence;
+
+import com.example.demo3.model.CommunityEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommunityRepository extends JpaRepository<CommunityEntity, Long> {
+}
