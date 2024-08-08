63a0c1aeb3986fbfb73102f21b018b57da6552b4
joshkim159 <joshkim159@hotmail.com>
2024-08-08 18:31:17 +0900

    2024-08-08 대여횟수, 최근 대여일,  관리자검색기능(백엔드만), 유저별 검색기능(프론트 백)

--- src/main/frontend/src/util/util.js
+++ src/main/frontend/src/util/util.js
@@ -18,4 +18,23 @@
     
     return `${year}-${month}-${day} ${hours}:${minutes}` //+ seconds;
   }
-  
+
+  export function toDateTime(dateArray) {
+    let now;
+    if (Array.isArray(dateArray) && dateArray.length >= 3) {
+        const [year, month, day, hours, minutes, seconds] = dateArray;
+        now = new Date(year, month - 1, day, hours || 0, minutes || 0, seconds || 0);
+    } else {
+        now = new Date();
+    }
+
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const day = now.getDate().toString().padStart(2, '0');
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}`; // + ':' + seconds;
+}
+
