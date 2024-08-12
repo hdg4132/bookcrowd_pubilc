7fa710a35f51827c2238ed4e672e30eab0b23d9f
hdg4132 <hdg4132@gmail.com>
2024-08-12 17:38:39 +0900

    위시리스트 기능추가

--- src/main/frontend/src/component/Rent_Admin/Rent_admin_return.js
+++ src/main/frontend/src/component/Rent_Admin/Rent_admin_return.js
@@ -3,6 +3,7 @@ import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 import "./Rent_admin.css";
 import axios from "axios";
 import Rent_item_return from "./Rent_item_return";
+import bookLogo from "../../assets/MainLogo.png";
 
 const Rent_admin_return = () => {
   const [posts, setPosts] = useState([]);
@@ -27,7 +28,7 @@ const Rent_admin_return = () => {
   return (
     <div className="Rent_Admin_List">
       <div className="Admin_side">
-        <div className="Admin_LOGO"> 사이트 로고 </div>
+      <div className="Admin_LOGO"> <img src={bookLogo}/> </div>
         <div className="Admin_side_link">
           <div>
             <a href="">회원관리</a>
